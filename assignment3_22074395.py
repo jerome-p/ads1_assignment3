d8590a9ef52faa87b5a82e1efe226ee432ded9c9
Jerome <pingjpr@gmail.com>
2024-01-02 18:57:52 +0000

    Added code for using errors py file. Selected two indicators for clustering, got labels to work for scatter plot. Added code that prints out what the kmeans label mean.

--- assignment3_22074395.py
+++ assignment3_22074395.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 import sklearn.metrics as skmet
 import numpy as np
 import scipy.optimize as opt
+import errors as err
 
 def load_data(dataset, country_list):
     """
     This function takes in a csv file and a list of countries that are of
     interest. Returns two dataframes one with the years as columns and the
-    other country names as columns.
+    other country names as columns. (Reused assignment_2's code to
+                                     load dataset')
 
     Parameters
     ----------
@@ -37,15 +39,6 @@ def subset_data(data,
                 indicator1,indicator2,indicator3,indicator4,
                 countries):
     
-    # Subsetting the dataframe based on the country names passed
-    #data = data[(data['Country Name'] == country1) |
-    #            (data['Country Name'] == country2) |
-    #            (data['Country Name'] == country3) |
-    #            (data['Country Name'] == country4) |
-    #            (data['Country Name'] == country5) |
-    #            (data['Country Name'] == country6)
-    #            ]
-
     # Selecting only the indicators needed from the dataset.
     data = data[
         (data['Indicator Name'] == indicator1) |
@@ -53,6 +46,7 @@ def subset_data(data,
         (data['Indicator Name'] == indicator3) |
         (data['Indicator Name'] == indicator4)
     ]      
+    
     result_df = pd.DataFrame()
     for country in countries:
         temp = data[data['Country Name'] == country]
@@ -68,33 +62,7 @@ def subset_data(data,
         
         
     
-    return data, temp, result_df
-
-def kmeans_cluster(ncluster, normalised_df, indicator1,indicator2):
-    
-    plt.figure()
-    
-    kmeans = cluster.KMeans(n_clusters=ncluster, n_init=20)
-    
-    kmeans.fit(normalised_df)
-    
-    labels = kmeans.labels_
-        
-    cen = kmeans.cluster_centers_
-    
-    xkmeans = cen[:,0]
-    ykmeans = cen[:,1]
-        
-    cm = plt.colormaps["Paired"]
-    
-    plt.scatter(normalised_df[indicator1],
-                normalised_df[indicator2], marker="o", c=labels,
-                cmap=cm)
-    plt.scatter(xkmeans, ykmeans, marker="d", label="kmeans centres", c='black' )
-    
-    plt.legend()
-    
-    return cen, labels, cm
+    return result_df
 
 
 def sil_score(normalised_df):
@@ -112,7 +80,89 @@ def sil_score(normalised_df):
         print("Nclusters: "+str(n))
         print(skmet.silhouette_score(normalised_df, labels))
         
+def generate_kmeans_cluster_plot(result,normalised_df,cluster_df, indicator_1, 
+                                 indicator_2, ncluster,
+                                 min_values, max_values):
+    
+    
+    kmeans = cluster.KMeans(n_clusters=ncluster, n_init=20)
+    
+    kmeans.fit(normalised_df)
+    
+    labels = kmeans.labels_
         
+    cen = kmeans.cluster_centers_
+    
+    xkmeans = cen[:,0]
+    ykmeans = cen[:,1]
+    
+    result['label'] = labels
+
+        
+    cm = plt.colormaps["Paired"]
+
+    
+    #cen, labels, cm = kmeans_cluster(ncluster, normalised_df,
+    #               indicator_1,
+    #               indicator_2)
+    
+    #xkmeans = cen[:,0]
+    #ykmeans = cen[:,1]
+    
+    plt.figure()
+    
+    plt.scatter(normalised_df[indicator_1], 
+                normalised_df[indicator_2], marker="o", c=labels,
+                cmap=cm)
+    plt.scatter(xkmeans, ykmeans, marker="d", label="kmeans centres", c='red' )
+
+    plt.legend()
+    plt.show()
+
+    #cluster_df['labels'] = labels
+
+    plt.figure()
+    
+    backscaled_centers = clst.backscale(cen, min_values, max_values)
+    x = backscaled_centers[:,0]
+    y = backscaled_centers[:,1]
+    
+    #plt.figure()
+    
+    
+    cluster_name = []
+    for i in range(ncluster):
+        cluster_name.append(result[result['label'] == i]['Country'].unique())
+      
+    country_list = []    
+    country_name = ""
+    for i in range(len(cluster_name)):
+        for j in range(len(cluster_name[i])):
+            country_name += cluster_name[i][j] + ", "
+        country_list.append(country_name)
+        country_name = ""
+        
+    # Printing what countries Kmeans has clustered together   
+    for i in range(len(country_list)):
+        print("Kmeans Label "+str(i)+": "+ country_list[i])
+            
+    for i in result['label'].unique():
+        plt.scatter(result[result['label'] == i][indicator_1],
+                    result[result['label'] == i][indicator_2],
+                    label=country_list[i])
+        
+    plt.scatter(x, y, marker="d", label="original centres", c='black' )
+    
+    plt.xlabel(indicator_1)
+    plt.ylabel(indicator_2)    
+    plt.legend()
+    plt.show()
+    
+    
+    
+    return cluster_df
+    
+
 def exp_growth(t, scale, growth):
     """ Computes exponential function with scale and growth as free parameters
     """
@@ -121,26 +171,10 @@ def exp_growth(t, scale, growth):
     
     return f
 
-def funct(t,s,k):
-    
-    x = s * (np.exp(k*t))
-    
-    return x
 
-
-def generate_line_plot(data, country, indicator, xlabel, ylabel, title):
+def curve_fit_plot(data, country, indicator, xlabel, ylabel, title):
     """
-    This function generates line plots for the given dataframe for a particular
-    indicator in the world bank dataset. It also requires a list of country
-    names.
-    Parameters
-    ----------
-    data : TYPE
-    countries : TYPE
-    indicator : TYPE
-    xlabel : TYPE
-    ylabel : TYPE
-    title : TYPE
+    
 
     Returns None.
 
@@ -154,24 +188,57 @@ def generate_line_plot(data, country, indicator, xlabel, ylabel, title):
     # Transposing the df again to makes years the index.
     subset_df = subset_df.T
 
-    # Plotting using the subset df.
-    # in the same figure.
-    plt.plot(subset_df[1:], label=country)
+    subset_df = subset_df.reset_index()
+    subset_df = subset_df.drop(index=0)
+    
+    subset_df = subset_df.rename(
+        columns={'index': 'Year', 
+                 country: indicator})
+    
+    
+    xdata = subset_df['Year'].astype(int)
+    ydata = subset_df[indicator].astype(float)
+    
+    param, pcovar = opt.curve_fit(exp_growth, xdata, ydata, p0=(0,0))
+        
+    subset_df['pop_exp'] = exp_growth(xdata, *param)
+    pop_exp = exp_growth(xdata, *param)
 
-    # Labelling
+    plt.figure()
+    plt.plot(subset_df['Year'],
+             subset_df[indicator], 
+             label='Data')
+    
+    plt.plot(subset_df['Year'], 
+             subset_df['pop_exp'], 
+             label='fit')
+    
     plt.xticks(rotation=90)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.title(title)
+    
     plt.legend()
+    plt.title('Trial 1')
+    plt.show()
+    
+    sigma = err.error_prop(xdata, exp_growth, param, pcovar)
 
-    # Saving the figure.
-    #plt.savefig('figures/line_graph_'+indicator+'.png',
-    #            bbox_inches='tight',
-    #            dpi=200)
+    low = pop_exp - sigma
+    up = pop_exp + sigma
+
+
+    plt.figure()
+    plt.title("exponenetial function error")
+    plt.plot(xdata, ydata, label="data")
+    plt.plot(xdata, pop_exp, label="fit")
+    # plot error ranges with transparency
+    plt.fill_between(xdata, low, up, alpha=0.5, color="red")
+
+    plt.legend(loc="upper left")
     plt.show()
 
-    return subset_df
+
+    return 
 
 
 def main():
@@ -184,23 +251,26 @@ def main():
         'climate_change.csv',
         countries)
 
-    subset_df,temp2, result = subset_data(wb_data_years,
+    result = subset_data(wb_data_years,
                 'CO2 emissions from solid fuel consumption (% of total)',
                 'Agriculture, forestry, and fishing, value added (% of GDP)',
                 'Urban population (% of total population)',
                 'Cereal yield (kg per hectare)',
                 countries)  
     
+    result = result.dropna(axis=0)
+    
     # This produces a heatmap using cluster_tools py file.
     # The generated plot had to be saved maunally from spyder IDE.
     clst.map_corr(result)
     
     # Choosing indicators based on the heatmap
+    indicator_2 = 'CO2 emissions from solid fuel consumption (% of total)'
     indicator_1 = 'Urban population (% of total population)'
-    indicator_2 = 'Cereal yield (kg per hectare)'
     
     cluster_df = result[[indicator_1, indicator_2]]
-        
+    #cluster_df = cluster_df.dropna(axis=0)
+    
     scaler_output = clst.scaler(cluster_df)
     
     print(scaler_output[0])
@@ -211,61 +281,22 @@ def main():
     max_values = scaler_output[2]
    
     sil_score(normalised_df)
+    n_cluster = 2
+    labeled_df = generate_kmeans_cluster_plot(result,normalised_df,cluster_df,
+                                              indicator_1, indicator_2, 
+                                 n_cluster, min_values, max_values)
     
-    cen, labels, cm = kmeans_cluster(6, normalised_df,
-                   indicator_1,
-                   indicator_2)
+    
+    #print(result[result['label'] == 1]['Country'].unique())
 
-    backscaled_centers = clst.backscale(cen, min_values, max_values)
-    x = backscaled_centers[:,0]
-    y = backscaled_centers[:,1]
-    
-    plt.figure()
-    plt.scatter(result[indicator_1], result[indicator_2],
-                c=labels,cmap=cm )
-    plt.scatter(x, y, marker="d", label="original centres", c='black' )
-    
-    plt.legend()
-    
-
-    indicator_df = generate_line_plot(wb_data_country ,
+    curve_fit_plot(wb_data_country,
                        'India',
                        'Urban population (% of total population)',
                        'Years','urbanpop','Trialdank')
-    indicator_df = indicator_df.reset_index()
-    indicator_df = indicator_df.drop(index=0)
-    
-    indicator_df = indicator_df.rename(
-        columns={'index': 'Year', 
-                 'India': 'Urban population (% of total population)'})
-    
-    xdata = indicator_df['Year'].astype(int)
-    ydata = indicator_df['Urban population (% of total population)'].astype(float)
-    
-    param, pcovar = opt.curve_fit(exp_growth, xdata, ydata, p0=(0,0))
-    
-    print(param, pcovar)
-    
-    indicator_df['pop_exp'] = exp_growth(xdata, *param)
-
-    plt.figure()
-    plt.plot(indicator_df['Year'],
-             indicator_df['Urban population (% of total population)'], 
-             label='Data')
-    
-    plt.plot(indicator_df['Year'], 
-             indicator_df['pop_exp'], 
-             label='fit')
-    plt.xticks(rotation=90)
-
-    plt.legend()
-    plt.title('Trial 1')
-    plt.show()
-
         
-    return wb_data_years, wb_data_country, subset_df, temp2, result, indicator_df
+    return wb_data_years, wb_data_country, result, labeled_df
 
 
 
 if __name__ == '__main__':
-    years, countries, susbset, fortemp, result, indicatordf = main()
+    years, countries, result, labeled = main()
